b00a211b302b4923bb8e204b019c9fe30c20d740
Ivan Akulov <gxoptg@gmail.com>
2020-04-15 00:06:50 +0300

    Update the contact section
    * Clarify contact details, per https://twitter.com/goodmarketinghq/status/1249647702071222273
    * Tune the text slightly

--- src/components/HomeSectionContact/index.tsx
+++ src/components/HomeSectionContact/index.tsx
@@ -1,22 +1,58 @@
+import { StaticQuery, graphql } from 'gatsby';
 import * as React from 'react';
-import { PromptContainer, Container, Link } from './styled';
+import { SharpImageFixed } from '../../types';
+import { PromptContainer, Container, ContactImage, Link } from './styled';
+
+interface ContactSectionData {
+  iamakulov: SharpImageFixed;
+}
 
 interface ContactSectionProps {
   className?: string;
 }
 
-const ContactSection = ({ className = '' }: ContactSectionProps) => (
+const ContactSection = ({
+  className = '',
+  data,
+}: ContactSectionProps & { data: ContactSectionData }) => (
   <div className={className}>
     <Container>
       <Link href="mailto:perf@3perf.com">perf@3perf.com</Link>
       <PromptContainer>
         <p>
-          Interested? We’d be glad to help. Just drop us an email, and we’ll get
-          back shortly.
+          Interested? We’d be glad to help. Drop us an email, and{' '}
+          <ContactImage
+            fixed={data.iamakulov.childImageSharp.fixed}
+            Tag="span"
+          />{' '}
+          Ivan will get back to you in 24 hours.
         </p>
       </PromptContainer>
     </Container>
   </div>
 );
 
-export default ContactSection;
+const ContactSectionWithQuery = (props: ContactSectionProps) => (
+  <StaticQuery
+    query={graphql`
+      fragment ContactImage on File {
+        childImageSharp {
+          fixed(width: 24, height: 24, quality: 75) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+
+      query {
+        iamakulov: file(
+          relativePath: { eq: "HomeSectionContact/iamakulov.jpg" }
+        ) {
+          ...ContactImage
+        }
+      }
+    `}
+    render={data => <ContactSection data={data} {...props} />}
+  />
+);
+
+export default ContactSectionWithQuery;
