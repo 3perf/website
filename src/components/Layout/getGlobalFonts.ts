44903aae029c1629de9b5e3dbd6147a7d2ec5203
Ivan Akulov <gxoptg@gmail.com>
2020-06-05 21:36:08 +0300

    Tune the webfonts approach
    - Remove woff fallbacks since woff2 is already supported in 94% of browsers
    - When optimizing fonts with subfont, create two sets for landing and content pages (instead of a separate set for every page of the site). This improves the navigation experience and speeds up the build

--- src/components/Layout/getGlobalFonts.ts
+++ src/components/Layout/getGlobalFonts.ts
@@ -1,20 +1,11 @@
-import firaCodeRegularWoff from '../../styles/webfonts/FiraCode-Regular.woff';
 import firaCodeRegularWoff2 from '../../styles/webfonts/FiraCode-Regular.woff2';
-import montserratBlackWoff from '../../styles/webfonts/Montserrat-Black.woff';
 import montserratBlackWoff2 from '../../styles/webfonts/Montserrat-Black.woff2';
-import montserratBoldWoff from '../../styles/webfonts/Montserrat-Bold.woff';
 import montserratBoldWoff2 from '../../styles/webfonts/Montserrat-Bold.woff2';
-import montserratItalicWoff from '../../styles/webfonts/Montserrat-Italic.woff';
 import montserratItalicWoff2 from '../../styles/webfonts/Montserrat-Italic.woff2';
-import montserratRegularWoff from '../../styles/webfonts/Montserrat-Regular.woff';
 import montserratRegularWoff2 from '../../styles/webfonts/Montserrat-Regular.woff2';
-import merriweatherBoldWoff from '../../styles/webfonts/merriweather-v19-cyrillic_latin-700.woff';
 import merriweatherBoldWoff2 from '../../styles/webfonts/merriweather-v19-cyrillic_latin-700.woff2';
-import merriweatherBoldItalicWoff from '../../styles/webfonts/merriweather-v19-cyrillic_latin-700italic.woff';
 import merriweatherBoldItalicWoff2 from '../../styles/webfonts/merriweather-v19-cyrillic_latin-700italic.woff2';
-import merriweatherItalicWoff from '../../styles/webfonts/merriweather-v19-cyrillic_latin-italic.woff';
 import merriweatherItalicWoff2 from '../../styles/webfonts/merriweather-v19-cyrillic_latin-italic.woff2';
-import merriweatherRegularWoff from '../../styles/webfonts/merriweather-v19-cyrillic_latin-regular.woff';
 import merriweatherRegularWoff2 from '../../styles/webfonts/merriweather-v19-cyrillic_latin-regular.woff2';
 
 const getGlobalFonts = () => `
@@ -25,8 +16,7 @@ const getGlobalFonts = () => `
     font-display: swap;
     src: local('Montserrat Regular'),
       local('Montserrat-Regular'),
-      url(${montserratRegularWoff2}) format('woff2'),
-      url(${montserratRegularWoff}) format('woff');
+      url(${montserratRegularWoff2}) format('woff2');
   }
 
   @font-face {
@@ -36,8 +26,7 @@ const getGlobalFonts = () => `
     font-display: swap;
     src: local('Montserrat Italic'),
       local('Montserrat-Italic'),
-      url(${montserratItalicWoff2}) format('woff2'),
-      url(${montserratItalicWoff}) format('woff');
+      url(${montserratItalicWoff2}) format('woff2');
   }
 
   @font-face {
@@ -47,8 +36,7 @@ const getGlobalFonts = () => `
     font-display: swap;
     src: local('Montserrat Bold'),
       local('Montserrat-Bold'),
-      url(${montserratBoldWoff2}) format('woff2'),
-      url(${montserratBoldWoff}) format('woff');
+      url(${montserratBoldWoff2}) format('woff2');
   }
 
   @font-face {
@@ -58,8 +46,7 @@ const getGlobalFonts = () => `
     font-display: swap;
     src: local('Montserrat Black'),
       local('Montserrat-Black'),
-      url(${montserratBlackWoff2}) format('woff2'),
-      url(${montserratBlackWoff}) format('woff');
+      url(${montserratBlackWoff2}) format('woff2');
   }
   
   @font-face {
@@ -69,8 +56,7 @@ const getGlobalFonts = () => `
     font-display: swap;
     src: local('Merriweather Regular'),
       local('Merriweather-Regular'),
-      url(${merriweatherRegularWoff2}) format('woff2'),
-      url(${merriweatherRegularWoff}) format('woff');
+      url(${merriweatherRegularWoff2}) format('woff2');
   }
   
   @font-face {
@@ -80,8 +66,7 @@ const getGlobalFonts = () => `
     font-display: swap;
     src: local('Merriweather Italic'),
       local('Merriweather-Italic'),
-      url(${merriweatherItalicWoff2}) format('woff2'),
-      url(${merriweatherItalicWoff}) format('woff');
+      url(${merriweatherItalicWoff2}) format('woff2');
   }
 
   @font-face {
@@ -91,8 +76,7 @@ const getGlobalFonts = () => `
     font-display: swap;
     src: local('Merriweather Bold'),
       local('Merriweather-Bold'),
-      url(${merriweatherBoldWoff2}) format('woff2'),
-      url(${merriweatherBoldWoff}) format('woff');
+      url(${merriweatherBoldWoff2}) format('woff2');
   }
 
   @font-face {
@@ -102,8 +86,7 @@ const getGlobalFonts = () => `
     font-display: swap;
     src: local('Merriweather Bold Italic'),
       local('Merriweather-BoldItalic'),
-      url(${merriweatherBoldItalicWoff2}) format('woff2'),
-      url(${merriweatherBoldItalicWoff}) format('woff');
+      url(${merriweatherBoldItalicWoff2}) format('woff2');
   }
 
   @font-face {
@@ -113,8 +96,7 @@ const getGlobalFonts = () => `
     font-display: optional;
     src: local('Fira Code Regular'),
       local('FiraCode-Regular'),
-      url(${firaCodeRegularWoff2}) format('woff2'),
-      url(${firaCodeRegularWoff}) format('woff');
+      url(${firaCodeRegularWoff2}) format('woff2');
   }
 `;
 
