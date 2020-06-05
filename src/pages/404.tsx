6048895992ea9e2ecca8fecd8df8a888b1c5a192
Ivan Akulov <gxoptg@gmail.com>
2020-06-05 16:33:10 +0300

    Tune the apostrophe in 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,7 +4,7 @@ import Layout from '../components/Layout';
 const NotFoundPage = () => (
   <Layout>
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>You just hit a route that doesn’t exist... the sadness.</p>
   </Layout>
 );
 
