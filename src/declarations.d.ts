f102ba149d257270990b1d589ddd5454b9537f45
Ivan Akulov <gxoptg@gmail.com>
2020-06-05 21:36:08 +0300

    Tune the webfonts approach
    - Remove woff fallbacks since woff2 is already supported in 94% of browsers
    - When optimizing fonts with subfont, create two sets for landing and content pages (instead of a separate set for every page of the site). This improves the navigation experience and speeds up the build

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -18,11 +18,6 @@ declare module '*.jpg' {
   export default content;
 }
 
-declare module '*.woff' {
-  const content: string;
-  export default content;
-}
-
 declare module '*.woff2' {
   const content: string;
   export default content;
