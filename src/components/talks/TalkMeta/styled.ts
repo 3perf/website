cd6a72de02d1e5618edc22f1934d163c238c4684
Vadzim Mashnitski <vadikmash@gmail.com>
2021-06-23 16:59:05 +0300

    feat: simplify layout

--- src/components/talks/TalkMeta/styled.ts
+++ src/components/talks/TalkMeta/styled.ts
@@ -3,43 +3,19 @@ import { linkActiveStyles, linkStyles } from '../../../styles/shared-styles';
 import { gridSize, sizes } from '../../../styles/variables';
 
 export const Container = styled.div`
-  display: inline-flex;
-  flex-wrap: wrap;
   margin-top: ${gridSize * 3}px;
   font-size: ${sizes.fontLarge}px;
-
-  > * {
-    margin: ${gridSize}px 0 ${gridSize}px ${gridSize * 0.75}px;
-
-    :first-child {
-      margin: ${gridSize}px 0;
-    }
-  }
 `;
 
-export const Date = styled.div``;
+export const Date = styled.time``;
 
-export const Link = styled.a`
-  display: flex;
-  border: none;
-  color: inherit;
-  align-items: center;
-
-  &:hover,
-  &:focus,
-  &:active {
-    color: inherit;
-  }
-`;
-
-export const Name = styled.div`
+export const AuthorLink = styled.a`
   ${linkStyles};
   font-weight: bold;
-  border: none;
 
-  ${Link}:hover &,
-  ${Link}:focus &,
-  ${Link}:active & {
+  :hover &,
+  :focus &,
+  :active & {
     ${linkActiveStyles};
   }
 `;
