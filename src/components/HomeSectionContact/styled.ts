20253c5427f1f364a276db303cf1e1712b480999
Ivan Akulov <mail@iamakulov.com>
2021-10-16 14:38:38 +0300

    Add an animation for the perf@3perf.com email

--- src/components/HomeSectionContact/styled.ts
+++ src/components/HomeSectionContact/styled.ts
@@ -1,5 +1,6 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import media from '../../styles/media';
+import { linkStyles, linkActiveStyles } from '../../styles/shared-styles';
 import { gridSize } from '../../styles/variables';
 import Image from '../Image';
 
@@ -10,6 +11,40 @@ export const Container = styled.div`
   border-radius: 4px;
   background: #ffdb01;
   color: #000;
+`;
+
+export const Dot = styled.span`
+  color: transparent;
+
+  &::before {
+    content: '.';
+    position: absolute;
+    text-decoration: none;
+  }
+`;
+
+const jump = keyframes`
+  0% {
+    transform: scale(1,1) translateY(0)
+  }
+  20% {
+    transform: scale(.95,1.05) translateY(-15px)
+  }
+  40% {
+    transform: scale(1.03,.97) translateY(0)
+  }
+  57% {
+    transform: scale(.97,1.03) translateY(-10px)
+  }
+  74% {
+    transform: scale(1.02,.98) translateY(0)
+  }
+  80% {
+    transform: scale(1.02,.98) translateY(0)
+  }
+  100% {
+    transform: scale(1,1) translateY(0)
+  }
 `;
 
 export const Link = styled.a`
@@ -23,6 +58,26 @@ export const Link = styled.a`
   ${media.small`
     font-size: calc(10vw);
   `};
+
+  ${Dot}::before {
+    ${linkStyles}
+    color: black;
+    border-bottom: none;
+  }
+
+  &:hover,
+  &:focus,
+  &:active {
+    ${Dot}::before {
+      ${linkActiveStyles}
+      border: none;
+
+      @media (prefers-reduced-motion: no-preference) {
+        animation: ${jump} 0.7s;
+        animation-timing-function: cubic-bezier(0.28, 0.84, 0.42, 1);
+      }
+    }
+  }
 `;
 
 export const PromptContainer = styled.div`
