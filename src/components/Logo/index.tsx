3e6ca1cf3e9e9e1309598a47bd506c3518fd3d4d
Ivan Akulov <mail@iamakulov.com>
2021-10-16 12:24:48 +0300

    Fix the logo animation not working

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -27,15 +27,10 @@ const Logo = ({
         color={logoKind === LogoKind.Black ? 'black' : 'white'}
       />
       {isPlayful && (
-        // Putting the script into <head> (using <Helmet>) because, otherwise, on a slow network,
-        // it blocks rendering *in the middle of rendering*. As in, the browser renders everything till the script –
-        // and then keeps the remaining part of the page blank for a few seconds.
-        // Ideally, this script should live in the end of <body>, but there’s no easy way
-        // to move it there automatically.
         <Helmet>
-          <Script
-            innerHTMLCode={`
-              document.addEventListener('DOMContentLoaded', () => {
+          <script>
+            {`
+              function initLogoAnimation() {
                 const playAnimation = () => document.querySelector('.js--site-logo').classList.add('js--site-logo_animation-enabled');
                 const stopAnimation = () => document.querySelector('.js--site-logo').classList.remove('js--site-logo_animation-enabled');
                 // This callback listens to all animationEnd events
@@ -51,9 +46,14 @@ const Logo = ({
                 document.querySelector('.js--site-logo').addEventListener('click', playAnimation);
                 document.querySelector('.js--site-logo').addEventListener('mouseenter', playAnimation);
                 document.querySelector('.js--site-logo').addEventListener('animationend', stopAnimationIfAllDone);
-              })
+              }
+
+              if (document.readyState === 'loading')
+                document.addEventListener('DOMContentLoaded', initLogoAnimation)
+              else
+                initLogoAnimation()
             `}
-          />
+          </script>
         </Helmet>
       )}
     </div>
