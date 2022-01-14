41aaf97974aa146e4e747510c7612a52e8b95cae
Ivan Akulov <mail@iamakulov.com>
2022-01-14 13:09:47 +0300

    GatsbyImage: remove outdated styles

--- src/components/Image/styled.ts
+++ src/components/Image/styled.ts
@@ -1,9 +1,3 @@
 import styled from 'styled-components';
 
-export const Container = styled.span`
-  display: inline-block;
-  /* Copy default gatsby-image’s styles – this helps to easily apply border-radius */
-  overflow: hidden;
-  /* Remove extra spacing below the image: https://stackoverflow.com/a/13961130 */
-  line-height: 0;
-`;
+export const Container = styled.span``;
