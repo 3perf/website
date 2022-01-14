e02e3289612973e450198b7e268119eab67f0e03
Vadzim Mashnitski <vadikmash@gmail.com>
2022-01-14 23:33:50 +0300

    So, I hope this works...

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -10,9 +10,18 @@ interface ImageProps {
 }
 
 const Image = ({ className, alt, loading, imageData }: ImageProps) => {
+  let isSafari = false;
+  if (typeof window !== 'undefined') {
+    isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+  }
+
   return (
-    <Container className={className}>
-      <GatsbyImage image={imageData} alt={alt} loading={'eager'} />
+    <Container className={className} isSafari={isSafari}>
+      <GatsbyImage
+        image={imageData}
+        alt={alt}
+        loading={!loading || isSafari ? 'eager' : loading}
+      />
     </Container>
   );
 };
--- src/components/Image/styled.ts
+++ src/components/Image/styled.ts
@@ -7,18 +7,22 @@ if (typeof window !== 'undefined') {
 
 console.log(isSafari);
 
-export const Container = styled.span`
+export const Container = styled.span<{ isSafari: boolean }>`
   display: inline-block;
   /* Copy default gatsby-image’s styles – this helps to easily apply border-radius */
   overflow: hidden;
   /* Remove extra spacing below the image: https://stackoverflow.com/a/13961130 */
   line-height: 0;
 
-  .gatsby-image-wrapper > img {
-    opacity: 1 !important;
-  }
+  ${({ isSafari }) =>
+    isSafari &&
+    css`
+      .gatsby-image-wrapper > img {
+        opacity: 1 !important;
+      }
 
-  .gatsby-image-wrapper > div {
-    opacity: 0 !important;
-  }
+      .gatsby-image-wrapper > div {
+        opacity: 0 !important;
+      }
+    `}
 `;
