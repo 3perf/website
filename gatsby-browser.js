357a488118ac1f06cc27e63f1e3898a00df7c1f7
Vadzim Mashnitski <vadikmash@gmail.com>
2022-01-14 20:52:02 +0300

    Another attempet to fix images in safari

---
+++ gatsby-browser.js
@@ -0,0 +1,11 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+
+export const onClientEntry = async () => {
+  if (typeof IntersectionObserver === 'undefined') {
+    await import('intersection-observer');
+  }
+};
