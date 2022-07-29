dc41e3d80a3cb4764d3682e5b9c5e3efdf1bb79f
Vadzim Mashnitski <vadikmash@gmail.com>
2022-07-29 18:43:48 +0300

    Add font subsetting via @fontsource

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,12 +3,10 @@ import { createGlobalStyle } from 'styled-components';
 import { linkActiveStyles, linkStyles } from '../../styles/shared-styles';
 import { colors, sizes } from '../../styles/variables';
 import { JSXChildrenProp } from '../../types';
-import getGlobalFonts from './getGlobalFonts';
+import './fonts';
 
 // tslint:disable-next-line no-unused-expression
 const GlobalStyle = createGlobalStyle`
-  ${getGlobalFonts()}
-
   *,
   *::before,
   *::after {
