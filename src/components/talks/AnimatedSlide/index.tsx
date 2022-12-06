4964e6446642efd94c895ff7450e055e354a9bc4
Ivan Akulov <mail@iamakulov.com>
2022-12-06 01:59:03 +0100

    Redesign the AnimatedSlide controls
    This:
    - tunes the visual design of AnimatedSlide controls
    - tunes the logic in the inline script:
      - uses `data-*` selectors instead of `#id` selectors (the latter ones are incorrect because there could be multiple AnimatedSlides in a document)
      - implements three separate buttons instead of one + controls their visibility via `display: none`. (this is beneficial because we can now use JSX inside the buttons. this also separates the view from the logic)
      - rewinds the animations using the Web Animations API instead of cloning the node
      - groups all code handling `autoplay` states into the same `if`
      - replaces `?.` in favor of `.` – it’s better to fail with an explicit error than to spend several hours debugging why a button doesn’t work

--- src/components/talks/AnimatedSlide/index.tsx
+++ src/components/talks/AnimatedSlide/index.tsx
@@ -3,7 +3,53 @@ import { FC, SVGProps } from 'react';
 import { Helmet } from 'react-helmet';
 import { JSXChildrenProp } from '../../../types';
 import { ImageWrapper, Text } from '../Slide/styled';
-import { Container, PlayControlButton } from './styled';
+import { Container, Controls, ControlButton } from './styled';
+
+// Icons from https://heroicons.com/. License: MIT
+const PlayIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    className="w-5 h-5"
+  >
+    <path
+      fillRule="evenodd"
+      d="M2 10a8 8 0 1116 0 8 8 0 01-16 0zm6.39-2.908a.75.75 0 01.766.027l3.5 2.25a.75.75 0 010 1.262l-3.5 2.25A.75.75 0 018 12.25v-4.5a.75.75 0 01.39-.658z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+const PauseIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    className="w-5 h-5"
+  >
+    <path
+      fillRule="evenodd"
+      d="M2 10a8 8 0 1116 0 8 8 0 01-16 0zm5-2.25A.75.75 0 017.75 7h.5a.75.75 0 01.75.75v4.5a.75.75 0 01-.75.75h-.5a.75.75 0 01-.75-.75v-4.5zm4 0a.75.75 0 01.75-.75h.5a.75.75 0 01.75.75v4.5a.75.75 0 01-.75.75h-.5a.75.75 0 01-.75-.75v-4.5z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+const ReplayIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    className="w-5 h-5"
+  >
+    <path
+      fillRule="evenodd"
+      d="M15.312 11.424a5.5 5.5 0 01-9.201 2.466l-.312-.311h2.433a.75.75 0 000-1.5H3.989a.75.75 0 00-.75.75v4.242a.75.75 0 001.5 0v-2.43l.31.31a7 7 0 0011.712-3.138.75.75 0 00-1.449-.39zm1.23-3.723a.75.75 0 00.219-.53V2.929a.75.75 0 00-1.5 0V5.36l-.31-.31A7 7 0 003.239 8.188a.75.75 0 101.448.389A5.5 5.5 0 0113.89 6.11l.311.31h-2.432a.75.75 0 000 1.5h4.243a.75.75 0 00.53-.219z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
 
 interface SlideProps {
   slideId: string;
@@ -27,7 +73,12 @@ const AnimatedSlide = ({
   autoplay = 'on-viewport-entry',
 }: SlideProps) => {
   return (
-    <Container className={className} id={slideId}>
+    <Container
+      // idle | running | paused | finished, to mimic https://developer.mozilla.org/en-US/docs/Web/API/Animation/playState
+      data-animation-state="idle"
+      className={className}
+      id={slideId}
+    >
       <div>
         <ImageWrapper
           href={`#${slideId}`}
@@ -35,7 +86,7 @@ const AnimatedSlide = ({
           isSectionHeader={isSectionHeader}
         >
           <Svg
-            id="svg-animation"
+            data-svg-animation
             style={{
               height: 'auto',
               display: 'block',
@@ -44,7 +95,17 @@ const AnimatedSlide = ({
           />
         </ImageWrapper>
         {hasControls && (
-          <PlayControlButton id="play-control-button">Play</PlayControlButton>
+          <Controls>
+            <ControlButton data-control-button="play">
+              <PlayIcon /> Play
+            </ControlButton>
+            <ControlButton data-control-button="pause">
+              <PauseIcon /> Pause
+            </ControlButton>
+            <ControlButton data-control-button="replay">
+              <ReplayIcon /> Replay
+            </ControlButton>
+          </Controls>
         )}
       </div>
       <Text isSectionHeader={isSectionHeader}>{children}</Text>
@@ -52,45 +113,38 @@ const AnimatedSlide = ({
         <script>
           {`
           (function() {
-            let isPaused = ${autoplay !== 'enabled' ? 'true' : 'false'};
-            let isFinished = false;
-            let isFirstEntry = true;
-
             function initAnimation() {
               const containerNode = document.querySelector('#${slideId}');
-              const svgNode = containerNode?.querySelector('#svg-animation');
-              const animatedElements = svgNode?.querySelectorAll('g g');
-              const playControlButton = containerNode?.querySelector('#play-control-button');
+              const svgNode = containerNode.querySelector('[data-svg-animation]');
+              const animatedElements = [...svgNode.querySelectorAll('g g')];
 
-              function changeAnimationPlayState(isPlayStatePaused) {
-                animatedElements?.forEach(function(element) {
-                  element.style.animationPlayState = isPlayStatePaused ? 'paused' : 'running';
+              function rewindAnimation() {
+                animatedElements.forEach(function(element) {
+                  element.getAnimations().forEach(function(animation) {
+                    animation.currentTime = 0;
+                  });
                 });
-                if (playControlButton) {
-                  playControlButton.innerText = isPlayStatePaused ? 'Play' : 'Pause';
-                }
-                isPaused = isPlayStatePaused
-              };
-              function togglePlayState() {
-                changeAnimationPlayState(!isPaused);
-              };
-              function restart() {
-                const clonedSvgNode = svgNode.cloneNode(true);
-                svgNode.parentElement.replaceChild(clonedSvgNode, svgNode);
-                playControlButton.removeEventListener('click', restart);
-
-                initAnimation();
               }
 
-              let observer;
-              if (${autoplay === 'on-viewport-entry' ? 'true' : 'false'}) {
+              function setAnimationState(animationState) {
+                containerNode.dataset.animationState = animationState;
+
+                animatedElements.forEach(function(element) {
+                  element.style.animationPlayState = animationState === 'running' ? 'running' : 'paused';
+                });
+              }
+
+              // Start the animation if necessary
+              if (${JSON.stringify(autoplay)} === 'on-viewport-entry') {
                 const threshold = 1;
-                observer = new IntersectionObserver(
+                const observer = new IntersectionObserver(
                   function(entries) {
                     entries.forEach(function(entry) {
-                      if (entry.intersectionRatio >= threshold && isFirstEntry) {
-                        togglePlayState();
-                        isFirstEntry = false;
+                      if (entry.isIntersecting) {
+                        setAnimationState('running');
+
+                        // Don’t trigger the animation anymore, even if the user scrolls back up
+                        observer.unobserve(entry.target);
                       }
                     });
                   },
@@ -98,31 +152,36 @@ const AnimatedSlide = ({
                 );
 
                 observer.observe(svgNode);
+              } else if (${JSON.stringify(autoplay)} === 'enabled') {
+                setAnimationState('running');
               }
 
-              let runningAnimationsCount = animatedElements?.length ?? 0;
+              // Switch the button to “Replay” when the animation is finished
+              animatedElements.forEach(function(element) {
+                element.addEventListener('animationend', function() {
+                  const allAnimationsFinished = animatedElements.every(function(element) {
+                    return element.getAnimations().every(function(animation) {
+                      return animation.playState === 'finished';
+                    });
+                  });
 
-              function onAnimationEnd() {
-                runningAnimationsCount = runningAnimationsCount - 1;
-
-                if (runningAnimationsCount === 0) {
-                  isFinished = true;
-                  if (playControlButton) {
-                    playControlButton.innerText = 'Replay'
-                    playControlButton.removeEventListener('click', togglePlayState);
-                    playControlButton.addEventListener('click', restart);
+                  if (allAnimationsFinished) {
+                    setAnimationState('finished');
                   }
-                }
-              };
-
-              animatedElements?.forEach(function(element) {
-                element.addEventListener('animationend', onAnimationEnd);
+                });
               });
-              if (playControlButton) {
-                playControlButton.addEventListener('click', togglePlayState);
-              }
 
-              changeAnimationPlayState(isPaused);
+              // Make buttons interactive
+              containerNode.querySelector('[data-control-button="play"]')?.addEventListener('click', function() {
+                setAnimationState('running');
+              });
+              containerNode.querySelector('[data-control-button="pause"]')?.addEventListener('click', function() {
+                setAnimationState('paused');
+              });
+              containerNode.querySelector('[data-control-button="replay"]')?.addEventListener('click', function() {
+                rewindAnimation();
+                setAnimationState('running');
+              });
             }
 
             if (document.readyState === 'loading') {
--- src/components/talks/AnimatedSlide/styled.ts
+++ src/components/talks/AnimatedSlide/styled.ts
@@ -1,23 +1,64 @@
 import styled from 'styled-components';
-import { Container as _Container } from '../Slide/styled';
+import { linkActiveStyles, linkStyles } from '../../../styles/shared-styles';
+import { gridSize, sizes } from '../../../styles/variables';
+import { Container as SlideContainer } from '../Slide/styled';
 
-export const Container = styled(_Container)``;
+export const Container = styled(SlideContainer)`
+  &[data-animation-state='idle'] {
+    [data-control-button='play'] {
+      display: flex;
+    }
+  }
 
-export const PlayControlButton = styled.span`
-  display: inline-block;
-  position: relative;
-  left: 50%;
-  transform: translateX(-50%);
-  padding-top: 5px;
-  text-align: center;
+  &[data-animation-state='running'] {
+    [data-control-button='pause'] {
+      display: flex;
+    }
+  }
 
-  font-size: 14px;
-  color: #06c;
+  &[data-animation-state='paused'] {
+    [data-control-button='play'] {
+      display: flex;
+    }
+  }
 
-  cursor: pointer;
-  user-select: none;
+  &[data-animation-state='finished'] {
+    [data-control-button='replay'] {
+      display: flex;
+    }
+  }
+`;
 
-  &:hover {
-    text-decoration: underline;
+export const Controls = styled.div`
+  margin-top: ${gridSize / 2}px;
+`;
+
+export const ControlButton = styled.button`
+  display: none;
+
+  // Mimic link styles but without the underline and blue color
+  --link-color: black;
+  --link-border-color: rgba(0, 0, 0, 0.25);
+  border: 1px solid;
+
+  ${linkStyles}
+
+  align-items: center;
+
+  margin-left: 2px;
+  padding: 2px 4px;
+  border-radius: 4px;
+  background: white;
+  font-size: ${sizes.fontSmall}px;
+
+  &:hover,
+  &:focus,
+  &:active {
+    ${linkActiveStyles}
+  }
+
+  svg {
+    width: 16px;
+    margin-right: 4px;
   }
 `;
