2575cc56745b35698dadcbd894665107896f09bf
Ivan Akulov <mail@iamakulov.com>
2025-12-30 12:11:15 +0100

    Fix embed videos broken as of ~2 months ago

---
+++ justfile
@@ -0,0 +1,14 @@
+dev:
+    yarn develop
+
+build-locally:
+    yarn build-locally
+
+build:
+    yarn build
+
+format:
+    yarn format
+
+lint:
+    yarn lint
--- src/components/talks/LiveDemo/index.tsx
+++ src/components/talks/LiveDemo/index.tsx
@@ -37,6 +37,7 @@ const LiveDemo = ({
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
         loading="lazy"
+        referrerPolicy="strict-origin-when-cross-origin"
       ></iframe>
     </Video>
   </Figure>
